f32816200262e61058beb53f17aa3c7a16884e18
Michael Surbey <ecwpz91@users.noreply.github.com>
2020-11-04 18:35:00 -0500

    Removed debug messages, added rolebinding function

--- common_functions/openshift101.sh
+++ common_functions/openshift101.sh
@@ -27,49 +27,64 @@ function openshift101-started(){
     done
     
     # Check if file has USERNAME variables defined
-    if cat $TFILE | grep -q "${USERNAME}"
-    then
+    if cat $TFILE | grep -q "${USERNAME}"; then
         if [ "${USERLANG}" = "java" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - Java edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - Java edition"  "TRUE"
+            echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - Java edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - Java edition"  "TRUE"
         fi
         
         if [ "${USERLANG}" = "python" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - Python edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - Python edition"  "TRUE"
+            echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - Python edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - Python edition"  "TRUE"
         fi
         
         if [ "${USERLANG}" = "javascript" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - Node.js edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - Node.js edition"  "TRUE"
+            echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - Node.js edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - Node.js edition"  "TRUE"
         fi
         
         if [ "${USERLANG}" = "php" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - PHP edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - PHP edition"  "TRUE"
+            echo -e "\e[0;32m${USERNAME} has started Getting Started with OpenShift - PHP edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - PHP edition"  "TRUE"
         fi
     else
         if [ "${USERLANG}" = "java" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - Java edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - Java edition"  "FALSE"
+            echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - Java edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - Java edition"  "FALSE"
         fi
         
         if [ "${USERLANG}" = "python" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - Python edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - Python edition"  "FALSE"
+            echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - Python edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - Python edition"  "FALSE"
         fi
         
         if [ "${USERLANG}" = "javascript" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - Node.js edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - Node.js edition"  "FALSE"
+            echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - Node.js edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - Node.js edition"  "FALSE"
         fi
         
         if [ "${USERLANG}" = "php" ]; then
-            [[ "$DEBUG" == true ]] && echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - PHP edition\e[0m"
-            update-report ${USERNAME}  "Getting Started with OpenShift - PHP edition"  "FALSE"
+            echo -e "\e[0;31m${USERNAME} has not started  Getting Started with OpenShift - PHP edition\e[0m"
+            update-report ${USERNAME}  "Lab 0 - Getting Started with OpenShift - PHP edition"  "FALSE"
         fi
     fi
     
     # Remove temporary file from directory
     rm -f $TFILE
+}
+
+function get-rolebinding() {
+    USERNAME=${1}
+    PROJECTNAME=${2}
+    MESSAGE=${3}
+    SEARCHVAL=${4:-view}
+    ROLE=$(oc get rolebindings -n ${PROJECTNAME} | grep ${SEARCHVAL}  | awk '{print $1}')
+    
+    if [ ! -z ${ROLE} ]; then
+        echo -e "\e[0;32m${USERNAME} has completed ${MESSAGE}\e[0m"
+        update-report  ${USERNAME}  "${MESSAGE}"  "TRUE"
+    else
+        echo -e "\e[0;31m${USERNAME} has not completed  ${MESSAGE}\e[0m"
+        update-report  ${USERNAME}   "${MESSAGE}"  "FALSE"
+    fi
 }
